a4d2bd621d8fe7f7e839647cb440b57550999431
Александр Жаворонок <C0baltt@live.com>
2020-09-30 22:57:08 +0300

    Добавьте файлы проекта.

---
+++ C0baltt.lesson10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0baltt.lesson10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //модификатор_доступа тип_возвращаемого_значения имя_метода(тип_аргумента1 имя аргумента1, тип_аргумента2 имя аргумента2)
+        public int PrintHello(string name)
+        { 
+         return 1;
+        }
+    }
+}
