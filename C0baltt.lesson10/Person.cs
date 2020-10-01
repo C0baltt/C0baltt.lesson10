0db976dfc5310057c6902aa36a8a59eac23c55e0
Александр Жаворонок <C0baltt@live.com>
2020-10-01 22:22:27 +0300

    1

---
+++ C0baltt.lesson10/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+
+namespace C0baltt.lesson10
+{
+    public class Person//инициализируем класс Person
+    {
+        public string SecondName { get; set; }//задаем свойства 
+
+        public string Name { get; set; }//задаем свойства 
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public Person(string secondName, string name)
+        {
+            SecondName = secondName;
+            Name = name;
+
+
+            X = 0;
+            Y = 0;
+        }
+
+
+    }
+}
--- C0baltt.lesson10/Program.cs
+++ C0baltt.lesson10/Program.cs
@@ -11,11 +11,19 @@ namespace C0baltt.lesson10
         static void Main(string[] args)
         {
 
+            var person1 = new Person("Иванов", "Иван");
+            var person2 = new Person("Петров", "Петр");
+
+            for (var i = 0; i < 10; i++)
+            {
+                var position = person1.Run();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
         }
         //модификатор_доступа тип_возвращаемого_значения имя_метода(тип_аргумента1 имя аргумента1, тип_аргумента2 имя аргумента2)
-        public int PrintHello(string name)
-        { 
-         return 1;
-        }
+        
     }
 }
